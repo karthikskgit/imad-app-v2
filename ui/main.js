bc38c54677affcadcb30358619db990452990682
karthikskgit <karthik.seetharaman@hotmail.com>
2017-04-18 15:38:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,19 @@ var counter=0;
 button.onclick=function(){
     
  counter=counter+1;
- var span=document.getElementtbyId('count')
-span.innerHTML=counter.toString()};
+ var request=new XMLHttpRequest();
+ request.onreadystatechange=fuction()
+ {
+     if(request,readyState====XMLHttpRequest.DONE)
+     {
+         if(request.status===200)
+         {
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+         
+         }
+     }
+ }
+ request.open('GET','http://http://karthikskgit.imad.hasura-app.io/counter',true);
+ request.send(null);
