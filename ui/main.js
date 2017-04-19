3f2df7c3cd76d297b08b976257cf673391031a27
karthikskgit <karthik.seetharaman@hotmail.com>
2017-04-19 11:00:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,3 +29,34 @@ button.onclick=function(){
  request.open('GET','http://http://karthikskgit.imad.hasura-app.io/counter',true);
  request.send(null);
 };
+
+var submit=docment.getElemenbyId('submit_btn');
+submit.onclick=funcction()
+{
+    var request=new XMLHttpRequest();;
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status==200)
+            {
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for (var i=0; i<names.length;i++){
+                    
+                    list+='<li>'+names[i]+'<li>';
+                }
+                var ul=document.getElementById('nammelist');
+                ul.innerHTML=list;
+            }}
+    };
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    request.open('GET','http://http://karthikskgit.imad.hasura-app.io:/submit-name?name' +name, true);
+    request.send(null);
+                };
+            }
+        }
+    }
+}
