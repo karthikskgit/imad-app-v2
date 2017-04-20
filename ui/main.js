f25d9999889beee1dae4d2d5fc5e31c8143a19e8
karthikskgit <karthik.seetharaman@hotmail.com>
2017-04-20 14:39:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,21 +2,7 @@ var button= document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
     
- 
-             
-         
-         
-     
- 
- var nameInput=document.getElementById('name');
- var name=nameInput.value;
- request.open('GET','http://http://karthikskgit.imad.hasura-app.io/counter',true);
- request.send(null);
-};
 
-var submit=docment.getElementById('submit_btn');
-submit.onclick=function()
-{
     var request=new XMLHttpRequest();
     
     request.onreadystatechange=function()
@@ -40,6 +26,40 @@ submit.onclick=function()
     var name=nameInput.value;
     request.open('GET','http://karthikskgit.imad.hasura-app.io:/submit-name?name' +name, true);
     request.send(null);
+    
+}
+
+var submit=docment.getElementById('submit_btn');
+submit.onclick=function()
+{
+
+ var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status==200)
+            {
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for (var i=0; i<names.length;i++){
+                    
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }}
+   
+
+
+var nameInput=document.getElementById('name');
+ var name=nameInput.value;
+ request.open('GET','http://http://karthikskgit.imad.hasura-app.io/counter',true);
+ request.send(null);
+};
+
                 
             
         
