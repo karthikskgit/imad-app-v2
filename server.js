03a90ba066aac4cb5ec8b02454ef8efab512b9d3
karthikskgit <karthik.seetharaman@hotmail.com>
2017-04-20 12:22:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').Pool;
+var crypto=require('crypto');
 
-
+var config= {
+    user:'karthikskgit',
+    database:'karthikskgit',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
 };
+
 
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate (data)  
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool=new Pool(config;
+app.get('/test-db',function(req,res)
+{
+    if(err)
+    {
+        res.status(500).send(err.toString());
+    }
+    else
+    {
+        res.send(JSON.striingify(result));
+    }
+    });
+    });
+    z
+}
+function hash(input,salt)
+{
+    var hashed=crypto.pdkdfSync(input,salt,1000,512,'sha512');
+    return hashed.toString('hex');
+    
+}
+
+app.get('/hash/:input', function(req,res){
+    var hashedString=hash(req.params.input,'this-is-some-string');
+    res.send(hashedString);
+});
+
+var pool=new Pool(config);
+app.get('/test-db', function(req,res){
+    
+}
 var counter=0;
   
 app.get('/counter', function(req,res) {
