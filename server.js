39040efbcbe6cddb15f2041bdd164bf4a47ab9a3
karthikskgit <karthik.seetharaman@hotmail.com>
2017-04-20 13:26:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=require('pg').Pool;
-var crypto=require('crypto');
+var app=express();
+app.use(morgan('combined'));
 
-var config= {
-    user:'karthikskgit',
-    database:'karthikskgit',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
-};
 
 
 var app = express();
@@ -22,50 +15,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool=new Pool(config;
-app.get('/test-db',function(req,res)
-{
-    if(err)
-    {
-        res.status(500).send(err.toString());
-    }
-    else
-    {
-        res.send(JSON.striingify(result));
-    }
-    });
-    });
-    z
-}
-function hash(input,salt)
-{
-    var hashed=crypto.pdkdfSync(input,salt,1000,512,'sha512');
-    return hashed.toString('hex');
-    
-}
 
-app.get('/hash/:input', function(req,res){
-    var hashedString=hash(req.params.input,'this-is-some-string');
-    res.send(hashedString);
-});
 
-var pool=new Pool(config);
-app.get('/test-db', function(req,res){
+
+
     
-}
+
 var counter=0;
   
 app.get('/counter', function(req,res) {
     counter=counter+1;
     res.send(counter.toString());
     
-});
-var names=[];
-app.het('/submit name',function(req,res){
-    var name=req.query.name;
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
 });
 app.get('/:articleName',function(req,res)
 {
@@ -85,6 +46,18 @@ app.get('/ui/main.js',function(req,res){
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui,madi.png'));
+});
+
+var names=[];
+app.get('submit-name/;name',function(req,res) {
+    // Getting name from requst
+    
+    var name=req.params.name;
+    
+    names.push(name);
+    // json obj notation
+    res.send(JSON.stringify(names));
+    
 });
 
 
