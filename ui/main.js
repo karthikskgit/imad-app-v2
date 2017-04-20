21bfadc61b0cc4a41ced86e24cc6a6bccd81d963
karthikskgit <karthik.seetharaman@hotmail.com>
2017-04-20 14:44:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 var button= document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
     
 
@@ -9,21 +9,15 @@ button.onclick=function(){
     {
         if(request.readyState===XMLHttpRequest.DONE)
         {
-            if(request.status==200)
+            if(request.status===200)
             {
-                var names=request.responseText;
-                names=JSON.parse(names);
-                var list='';
-                for (var i=0; i<names.length;i++){
-                    
-                    list+='<li>'+names[i]+'</li>';
-                }
-                var ul=document.getElementById('namelist');
-                ul.innerHTML=list;
+                var counter=request.responseText;
+                var spa=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
             }}
     };
-    var nameInput=document.getElementById('name');
-    var name=nameInput.value;
+   
     request.open('GET','http://karthikskgit.imad.hasura-app.io:/submit-name?name' +name, true);
     request.send(null);
     
