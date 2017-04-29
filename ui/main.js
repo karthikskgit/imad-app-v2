69200206cc66c0f6ca87e762c5d1a35b0cdf41bd
karthikskgit <karthik.seetharaman@hotmail.com>
2017-04-29 21:16:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,7 +50,6 @@ submit.onclick = function()
             var ul=document.getElementById('namelist');
             ul.innerHTML = list;
                 
-                
             }
             
         }
