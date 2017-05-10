ba8f75575bb829aa6e839110022a2fc02eb2fa77
karthikskgit <karthik.seetharaman@hotmail.com>
2017-05-10 16:33:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var  Pool=require('pg').Pool;
+var Pool=require('pg').Pool;
 
 var config={
     user:'karthikskgit',
@@ -12,22 +12,10 @@ var config={
 };
 
 
-
-
-
-
-
-
-
 var app = express();
-
-
-
-
-
-
-
 app.use(morgan('combined'));
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
